9b66ea55a0aba96b7dfc5042f13014452a20f458
SerobSayadyan <serobsayadyan97@gmail.com>
2023-05-27 17:07:13 +0400

    improvements

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import organizer.Organizer;
 
 public class Main {
     public static void main(String[] args) {
-        long start = System.nanoTime();
         Organizer.organize();
-        long end = System.nanoTime();
-//        System.out.println("\n\nruning time" + (end - start));
     }
 }
